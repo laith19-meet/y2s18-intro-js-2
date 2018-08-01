096834e4c67b77b7e04c17ee98aa89cf7428a1f7
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 16:53:11 +0300

    finisheeeeedddd

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,8 @@
 // Put all your JavaScript in this file!
+
+function changebackground (color){
+		var click = prompt("choose a color");
+		var rr = document.getElementsByTagName("body")
+	 rr[0].style.backgroundColor= click;
+}
+
